e02e04810ec103f159d3bd58d5f5e8b7bfae157d
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-06-05 17:27:10 +0100

    redux

---
+++ src/state/action-creators/index.js
@@ -0,0 +1,19 @@
+// houses all of our actions
+
+export const  depositMoney = (amount) => {
+    return (dispatch) => {
+        dispatch({
+            type: "deposit",
+            payload: amount
+        })
+    }
+}
+
+export const withdrawMoney = (amount) => {
+    return (dispatch) => {
+        dispatch({
+            type: "withdraw",
+            payload : amount
+        })
+    }
+}
